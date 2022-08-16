9efd2257455a56b7d614276282ba2a727590d86d
joan <joanflores905@gmail.com>
2022-08-16 19:42:35 +0000

    version 2

--- src/app.js
+++ src/app.js
@@ -5,7 +5,43 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+function randomStuff(max) {
+  return Math.floor(Math.random() * max);
+}
+
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  const signTop = document.querySelector(".sign-top");
+  const cardText = document.querySelector(".card-t");
+  const signBot = document.querySelector(".sign-bot");
+
+  let number = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let pinta = ["♦", "♥", "♠", "♣"];
+
+  let blackOrRed = randomStuff(4);
+  let numberRandom = number[randomStuff(number.length)];
+  let pintaRandom = pinta[blackOrRed];
+
+  if (blackOrRed == 0 || blackOrRed == 1) {
+    signTop.classList.add("red");
+    signBot.classList.add("red");
+  }
+
+  signTop.textContent = pintaRandom;
+  cardText.textContent = numberRandom;
+  signBot.textContent = pintaRandom;
 };
